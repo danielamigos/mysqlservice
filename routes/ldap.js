d4af214b41a09c9380af5262d9461315e038d796
Daniel Valenzuela Lemus <daniel@processtempo.com>
2020-02-20 17:08:57 -0600

    try catch

--- routes/ldap.js
+++ routes/ldap.js
@@ -6,16 +6,22 @@ var dn = 'fordGID=2178281,OU=Employee,OU=People,O=ford,C=US';
 var pass = 'WYEM=7naft';
 
 router.get('/bind', function (req, res) {
+    console.log('first')
+    try {
+        var client = ldap.createClient({
+            url: req.query.url
+        });
+        console.log('second')
+        client.bind(dn, pass, function (err, ldapRes) {
+            console.log('third')
+            assert.ifError(err);
+            res.send(ldapRes);
+        });
 
-    var client = ldap.createClient({
-        url: req.query.url
-    });
-
-    client.bind(dn, pass, function (err, ldapRes) {
-        assert.ifError(err);
-        res.send(ldapRes);
-        return;
-    });
+    } catch (ex) {
+        console.log('forth')
+        res.send(ex);
+    }
 
 });
 
